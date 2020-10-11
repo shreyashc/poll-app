aa8beeac02586939b09769fec585ac647e52ad50
shreyashc <shreyashc018@gmail.com>
2020-10-11 20:40:51 +0530

    Updates

---
+++ precache-manifest.87ea1a52b64b7e3328d4e53c58156f30.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a13bde4e79a4d00919007a5f6be637ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b146d1fcae129b2296d1",
+    "url": "/static/css/main.c6bcd1d0.chunk.css"
+  },
+  {
+    "revision": "67cc3f187123e8fce61d",
+    "url": "/static/js/2.2c40fb94.chunk.js"
+  },
+  {
+    "revision": "074c14ad73a132a8644d7b16e268f298",
+    "url": "/static/js/2.2c40fb94.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b146d1fcae129b2296d1",
+    "url": "/static/js/main.51921b79.chunk.js"
+  },
+  {
+    "revision": "ebbef11a5acd700fd48c",
+    "url": "/static/js/runtime-main.bf7efd6c.js"
+  },
+  {
+    "revision": "4bbfc017468a6959489cb9c873a5b01b",
+    "url": "/static/media/chart.4bbfc017.svg"
+  }
+]);
