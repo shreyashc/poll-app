5b25ab0676a05649eb816d0f286081fd8e0270da
shreyashc <shreyashc018@gmail.com>
2020-10-11 22:15:12 +0530

    refactoring

--- poll-frontend/src/pages/Success/Success.jsx
+++ poll-frontend/src/pages/Success/Success.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./style.scss";
+import { FRONTEND_URL } from "../../utils/constants";
 const Success = () => {
     return (
         <div className="success-container">
             <h1>Your response was recorded</h1>
             <h4>
-                create your own poll <a href="/">here</a>
+                create your own poll <a href={FRONTEND_URL}>here</a>
             </h4>
         </div>
     );
